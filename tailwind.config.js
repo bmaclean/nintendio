d4556d9d257bd993773785684cad5dfdd7070f77
Brendan Maclean <brendan@ignitertickets.com>
2020-11-21 22:45:49 -0700

    App setup: deps, ESlint, config files

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      header: ['Black Ops One'],
+    },
+    extend: {
+      transitionProperty: {
+        height: 'height',
+        visibility: 'visibility',
+        'margin-bottom': 'margin-bottom',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
